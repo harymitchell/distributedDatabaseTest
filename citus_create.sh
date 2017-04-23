691dba4243a0510bd8d04a63bc341da3d2e7376c
hmitchell <harrison.mitchell@coxautoinc.com>
2017-04-23 20:07:36 +0000

    Updates

--- citus_create.sh
+++ citus_create.sh
@@ -10,5 +10,6 @@ psql template1 -c "create user northwind_user;"
 psql template1 -c "alter user northwind_user password 'thewindisblowing';"
 psql template1 -c "grant all on DATABASE northwind to northwind_user;"
 psql northwind -c "GRANT ALL on ALL tables IN SCHEMA public to northwind_user"
+psql northwind -c "GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO northwind_user;"
 
-psql northwind < /data/base/distributedDatabaseTest/distributeCitus.sql
+psql northwind < /data/base/distributedDatabaseTest/citusDistribution.sql
--- distributeCitus.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-psql northwind < ~/distributedDatabaseTest/citusDistribution.sql
