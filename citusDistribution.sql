a1dac1f9c959af10497a9e1122ba9e31e39eab6a
hmitchell <harrison.mitchell@coxautoinc.com>
2017-04-23 23:42:44 +0000

    Updates

--- citusDistribution.sql
+++ citusDistribution.sql
@@ -267,11 +267,11 @@ SELECT create_distributed_table('customerdemographics', 'customertypeid');
 SELECT create_distributed_table('customers', 'customerid');
 SELECT create_distributed_table('employees', 'employeeid');
 SELECT create_distributed_table('employeeterritories', 'employeeid');
-SELECT create_distributed_table('order_details', 'orderid');
-SELECT create_distributed_table('orders', 'orderid', colocate_with => 'order_details');
+SELECT create_distributed_table('order_details', 'orderid', colocate_with => 'customers');
+SELECT create_distributed_table('orders', 'orderid', colocate_with => 'customers');
 SELECT create_distributed_table('products', 'productid');
 SELECT create_distributed_table('shippers', 'shipperid');
-SELECT create_distributed_table('suppliers', 'supplierid');
+SELECT create_distributed_table('suppliers', 'supplierid', colocate_with => 'products');
 
 SELECT create_reference_table('region');
 SELECT create_reference_table('territories');
